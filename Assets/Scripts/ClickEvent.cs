b025934e8dcf4118dbdbc2c566b805aaea7e0490
Keisuke Yasuda <keisuke0319@gmail.com>
2014-12-19 00:32:54 +0900

    Create New Scripts and Optimize for Nexus7

--- Assets/Scripts/ClickEvent.cs
+++ Assets/Scripts/ClickEvent.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class ClickEvent : MonoBehaviour {
 
 	public CountManager cManager;
+    public GameObject[] dropObject = new GameObject[1];
+    public GameObject parentObject;
+    private int randObjNum;
 
 	// Use this for initialization
 	void Start () {
@@ -17,5 +20,16 @@ public class ClickEvent : MonoBehaviour {
 		cManager.sum += cManager.perClick;
 		cManager.clickSum += cManager.perClick;
 		cManager.clickedNum += cManager.perClick;
+        randObjNum = Random.Range(0, dropObject.Length);
+        LocalInstantiate(randObjNum);
 	}
+
+    void LocalInstantiate(int dropObjectNum) {
+        float randPos;
+        GameObject tempObject;
+        randPos = Random.Range(-9.0f, 9.0f);
+        tempObject = Instantiate(dropObject[dropObjectNum]) as GameObject;
+        tempObject.transform.position = new Vector3(randPos, 22, 0);
+        tempObject.transform.parent = parentObject.transform;
+    }
 }
